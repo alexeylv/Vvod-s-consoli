862aa3695c6c16c3ebe6af44a3910b9fc2948d58
alexeylv <101318447+alexeylv@users.noreply.github.com>
2024-01-08 15:09:02 +0000

    изменить последнюю строку. Замены на "Ok"

--- .ccls-cache/@home@runner@Vvod-s-consoli/main.cpp
+++ .ccls-cache/@home@runner@Vvod-s-consoli/main.cpp
@@ -2,8 +2,8 @@
 
 int main() {
   int num;
-  std::cout << "Hello World!" << std::endl;
+  std::cout << "Hello World and Git!" << std::endl;
   std::cin >> num;
   std::cout << "Your number is " << num << std::endl;
-  std::cout << "Integrate GitHub/n" << std::endl;
+  std::cout << "Ok" << std::endl;
 }
