2488a2182afb14dd60ca6e76717d3981a56567f4
alexeylv <101318447+alexeylv@users.noreply.github.com>
2024-01-08 15:01:30 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Vvod-s-consoli/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+  int num;
+  std::cout << "Hello World!" << std::endl;
+  std::cin >> num;
+  std::cout << "Your number is " << num << std::endl;
+  std::cout << "Integrate GitHub/n" << std::endl;
+}
